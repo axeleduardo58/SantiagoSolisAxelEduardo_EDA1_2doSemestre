089eceec2ea112ce5273405e47f57945e2578423
axeleduardo58 <axel07santiago@gmail.com>
2026-05-17 14:27:21 -0600

    Add files via upload

---
+++ Practica10/Ejercicio2/bubble.h
@@ -0,0 +1,9 @@
+/* Práctica: 10 Estrategia para la contrucción de algoritmos I
+Realizado por: Primero Reyes Monserrath, Ortiz Hernández Yesica, Santiago Solís Axel Eduardo, De la Cruz Flores Natalia.
+El archivo bubble.h declara la función bubbleSort, permitiendo que otros archivos puedan utilizarla. Además, 
+utiliza guardas de inclusión para evitar errores por múltiples inclusiones. */
+
+#ifndef BUBBLE_H //Si BUBBLE_H NO esta incluido
+#define BUBBLE_H // Nos marca si BUBBLE_H ya se incluyo
+void bubbleSort(int arr[], int n); //Valida que exista la funcion bubbleSort, recibiendo su arrego y tamaño.
+#endif
